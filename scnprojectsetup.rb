eefa68e9c44cc6b2a716f57f9fd642725e2d48a8
Sebastian Antonsen <sebastian@stormstudios.no>
2014-11-10 16:04:00 +0100

    added homebrew formulae

---
+++ scnprojectsetup.rb
@@ -0,0 +1,20 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Scnprojectsetup < Formula
+  homepage "http://shortcutolso.no"
+  url "https://github.com/santonsen/homebrew-scnprojectsetup/blob/master/dist/scnprojectsetup-0.0.1.tar.gz"
+  sha1 "c8aa29ae7884f2b706fa1130d187d93c0cf7592a"
+
+  def install
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "python setup.py install"
+  end
+end
