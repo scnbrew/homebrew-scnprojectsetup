18abc70a1e2695cc97db64ed97a1c18d63b42b17
Sebastian Antonsen <sebastian@stormstudios.no>
2014-11-25 20:23:26 +0100

    and now I ran make dist for 1.0.0. as well

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_0.0.3/scnprojectsetup_0.0.3.tar.gz"
-  sha1 "c3405ee79cd9a475727b9598e882c12751009b5d"
+  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.0.0/scnprojectsetup_1.0.0.tar.gz"
+  sha1 "2d869c62995e19b618154c1221e10951402c200e"
 
   def install
     # Remove unrecognized options if warned by configure
