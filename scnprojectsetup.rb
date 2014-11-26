d702b239244dd77a47df05d6d3ef7ca1243df0c0
Sebastian Antonsen <sebastian@stormstudios.no>
2014-11-26 12:08:21 +0100

    destination is now a required argument there is no longer a default destination the empty file named after the project now has the .txt extension if too few arguments are passed the help message is returned along with the error message an example is shown after usage: usage now prints the required arguments first, the optionals second

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
   url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.0.0/scnprojectsetup_1.0.0.tar.gz"
-  sha1 "2d869c62995e19b618154c1221e10951402c200e"
+  sha1 "93fa41516760562caa9001f38cc2d1576529553f"
 
   def install
     # Remove unrecognized options if warned by configure
