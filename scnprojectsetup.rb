6073ad25ffe3d47017daa286301ad9f5e80a9c8e
Sebastian Antonsen <sebastian@stormstudios.no>
2015-01-22 12:14:18 +0100

    built version 1.3.1. Ready for push

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.2.1/scnprojectsetup_1.2.1.tar.gz"
-  sha1 "2d5a7ad530abe71354cce343abd337231bd87567"
+  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.3.0/scnprojectsetup_1.3.0.tar.gz"
+  sha1 "179c8b9841dc36021e8361292ba8cc1880aeeb36"
 
   def install
     # Remove unrecognized options if warned by configure
