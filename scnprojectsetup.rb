e94dabbfeca82e35de9a1dcee5d9818a4455bb8b
Sebastian Antonsen <sebastian@stormstudios.no>
2015-02-09 17:09:57 +0100

    added client_relations/to_client to com config. removed /offline/aaf/ and /offline/aaf/ from com omit

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.4.0/scnprojectsetup_1.4.0.tar.gz"
-  sha1 "92aaed22e19a71a35b67e60f8aa42255dce1318a"
+  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.4.1/scnprojectsetup_1.4.1.tar.gz"
+  sha1 "8bad71ba9d4f5a89e58518ff2167e7aa55e0da04"
 
   def install
     # Remove unrecognized options if warned by configure
