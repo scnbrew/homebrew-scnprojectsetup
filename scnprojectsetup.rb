541712e4a50a319ab28a946aea6509b0c6f2e4dc
Sebastian Antonsen <sebastian@stormstudios.no>
2015-02-10 16:33:31 +0100

    changed online folder config for commercial

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.4.1/scnprojectsetup_1.4.1.tar.gz"
-  sha1 "8bad71ba9d4f5a89e58518ff2167e7aa55e0da04"
+  url "https://github.com/santonsen/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.4.2/scnprojectsetup_1.4.2.tar.gz"
+  sha1 "6a8fde77f2a690f9394251f02f2ff282f99e14b2"
 
   def install
     # Remove unrecognized options if warned by configure
