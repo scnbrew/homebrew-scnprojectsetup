a3ddcbcd0f733b252e56ec6ae3da3f85dfcfa130
Sebastian Antonsen <sebastian@tubatools.com>
2016-10-28 18:32:40 +0200

    changed url temporarliy to git user tubasatan for testing

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.6.0/scnprojectsetup_1.6.0.tar.gz"
+  url "https://github.com/tubasatan/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.6.0/scnprojectsetup_1.6.0.tar.gz"
   sha256 "57c49225491aebf08e0bf688ecccebedd716a9df107635a5c363dddf59ea1b4d"
 
   def install
