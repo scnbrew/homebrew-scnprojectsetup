9309287a1d5b7c535f603e63af60ba6cda3569a2
psvennevig <psvennevig@scn-paslap.local>
2018-10-05 19:27:30 +0200

    Built 1.6.1 beta

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.6.0/scnprojectsetup_1.6.0.tar.gz"
-  sha256 "57c49225491aebf08e0bf688ecccebedd716a9df107635a5c363dddf59ea1b4d"
+  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.6.1/scnprojectsetup_1.6.1.tar.gz"
+  sha256 "a6481e998398fdba359432fc4b7c05bf0d6b74199ad08642c85b0eb044eb78c2"
 
   def install
     # Remove unrecognized options if warned by configure
