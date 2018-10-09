cdb044785f5d6f856f3793cff4b787191895f3c6
psvennevig <psvennevig@scn-paslap.stormstudio.no>
2018-10-09 12:10:26 +0200

    syntax error fix

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.6.3/scnprojectsetup_1.6.3.tar.gz"
-  sha256 "857679bf69a6ae489134a87367aadcb4dfecd08125ee4fd051af36b115ffe0f8"
+  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.6.4/scnprojectsetup_1.6.4.tar.gz"
+  sha256 "a36434d97ddd598e2789fe6e61b170f5779b3ef404313cf92e3bc310724e939f"
 
   def install
     # Remove unrecognized options if warned by configure
