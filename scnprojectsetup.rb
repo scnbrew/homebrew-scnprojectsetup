91cd1f5e53d2d610da42d3be5b4514a1e9f5fb7a
psvennevig <psvennevig@scn-paslap.local>
2020-03-04 16:39:53 +0100

    v 1.7.2

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.7.1/scnprojectsetup_1.7.1.tar.gz"
-  sha256 "2ad698a6c965993f72f0027a5a19604691350dfb7dc9a6606457ec8fbd64cff7"
+  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.7.2/scnprojectsetup_1.7.2.tar.gz"
+  sha256 "97f1dcfbb26d6f159925f14b85eabb151d5a1280714fb1f55644a662dd9e9bd8"
 
   def install
     # Remove unrecognized options if warned by configure
