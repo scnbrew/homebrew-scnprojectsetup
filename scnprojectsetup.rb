e8ece539e8602950c22aa790138d3f059d8a7390
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2023-12-21 21:55:41 +0100

    v1.8.0 update to python3

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
   url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.8.0/scnprojectsetup_1.8.0.tar.gz"
-  sha256 "633192b7837c1565c4af97e563b7e5ca170e7a57bddbe87e3d2ff4bc1a3e0898"
+  sha256 "c79fb419d5d25dd8d6853e934aaca13cfd7390366e583fc21869e0952215a9d1"
 
   def install
     # Remove unrecognized options if warned by configure
