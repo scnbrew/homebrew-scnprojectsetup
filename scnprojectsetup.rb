59e46fb43b291cfc79f836460df0fc70cb4818bc
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2025-10-23 16:36:03 +0200

    Bump version to 1.9.3 and update related files

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.2/scnprojectsetup_1.9.2.tar.gz"
-  sha256 "cef953447bd669780668b7649e96a6b564dbdcc6932cf32f22173fb066f9dfc7"
+  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.3/scnprojectsetup_1.9.3.tar.gz"
+  sha256 "2a64fdf601dd518c0093300a4c7d0996d0855ba206a2210492f912ebd01f23c9"
 
   def install
     # Remove unrecognized options if warned by configure
