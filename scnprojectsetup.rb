9e885422bec27870cc00ea6e20d4ae9448eda8f2
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2026-03-23 11:41:03 +0100

    refactor: Align with production workflows

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
   url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.4/scnprojectsetup_1.9.4.tar.gz"
-  sha256 "ed73affef06b56827170a5bf79752f1d990f202c55e6c8c2a1f4b7f97379bee4"
+  sha256 "eb0a56b8218ee8a327b0edb91b32c7f380478d7e6ffecaf6ffdafb641a03a05d"
 
   def install
     # Remove unrecognized options if warned by configure
