9addc5feb6d8ef05ee6e5035cedac001bd165569
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2026-03-23 11:27:16 +0100

    refactor: implemented new edit project type and pathing

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
   url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.4/scnprojectsetup_1.9.4.tar.gz"
-  sha256 "7dd45c42d9886a47cbd9af496a0633772f99ab91ebb4b44862be95f50156bca7"
+  sha256 "ed73affef06b56827170a5bf79752f1d990f202c55e6c8c2a1f4b7f97379bee4"
 
   def install
     # Remove unrecognized options if warned by configure
