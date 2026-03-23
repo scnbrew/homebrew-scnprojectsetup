cdf2a123a80538b43639484b233275fbc4cca385
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2026-03-23 16:22:35 +0100

    fix: refine folder handling for project types

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
   url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.5/scnprojectsetup_1.9.5.tar.gz"
-  sha256 "92aa78f818d8350e581c944d0f14fa614f1cab1b355fd54f09cf2a1db43b7594"
+  sha256 "9697fe6321f141a948508a24762841d055e1052abefd8c6d4da82e07fcc1e2e4"
 
   def install
     # Remove unrecognized options if warned by configure
