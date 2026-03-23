36a437ec9936c9087ea08a462d73a847d78aed63
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2026-03-23 15:06:03 +0100

    chore: Bump version to 1.9.5 and update related files

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
-  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.4/scnprojectsetup_1.9.4.tar.gz"
-  sha256 "af546a839d4d2b42bd532cf0e02a17d6c90c134c3f23cff661f6740c42c2aba4"
+  url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.5/scnprojectsetup_1.9.5.tar.gz"
+  sha256 "92aa78f818d8350e581c944d0f14fa614f1cab1b355fd54f09cf2a1db43b7594"
 
   def install
     # Remove unrecognized options if warned by configure
