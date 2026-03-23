bc5d6250cc3b011f997fca7e9124bde90b5649fc
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2026-03-23 11:17:52 +0100

    feat: added edit project creation and updated docs

--- scnprojectsetup.rb
+++ scnprojectsetup.rb
@@ -3,7 +3,7 @@ require "formula"
 class Scnprojectsetup < Formula
   homepage "http://shortcutolso.no"
   url "https://github.com/scnbrew/homebrew-scnprojectsetup/raw/master/dist/scnprojectsetup_1.9.4/scnprojectsetup_1.9.4.tar.gz"
-  sha256 "f6347d11f4bc143fdf539a12834b7c96093488d6c30fceb26f42dcdc71e85caf"
+  sha256 "7dd45c42d9886a47cbd9af496a0633772f99ab91ebb4b44862be95f50156bca7"
 
   def install
     # Remove unrecognized options if warned by configure
